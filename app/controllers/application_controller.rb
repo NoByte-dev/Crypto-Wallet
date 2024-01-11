97d3bb28b2430df726eadeb805eb0acfc68f5acf
NoByte <nobyte_dev@hotmail.com>
2024-01-11 09:45:47 -0300

    feat: Implementa mudança de idioma atravêz de cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    update_locale(params[:locale] || cookies[:locale])
+  end
+
+  def update_locale(locale)
+    return if locale.blank? || I18n.locale == locale
+
+    cookies[:locale] = locale
+    I18n.locale = locale
+  end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,4 +2,4 @@
 I18n.available_locales = ['en-US', 'pt-BR']
 
 # Set default locale to something other than :en
-I18n.default_locale = 'en-US'
+I18n.default_locale = 'pt-BR'
