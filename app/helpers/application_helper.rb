aef717c7d817a17393e028f1b38eeb83ddaf879c
NoByte <nobyte_dev@hotmail.com>
2024-01-12 06:38:23 -0300

    style: Nova página inicial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def locale_is
-    I18n.locale.to_s == 'pt-BR' ? 'Português do Brasil' : 'Inglês dos EUA'
-  end
+  def locale_set_flag
+    I18n.locale.to_s == 'pt-BR' ? '&#x1F1E7&#x1F1F7'.html_safe : '&#x1F1FA&#x1F1F8'.html_safe
+  end  
 end
