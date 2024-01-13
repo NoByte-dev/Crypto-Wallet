b8619b223442b4d555296dc6a86f96de0ee92dff
NoByte <nobyte_dev@hotmail.com>
2024-01-13 05:45:51 -0300

    feat: Implementa novas cores ao tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     './app/views/**/*',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cw-purple': {
+          100: '#f4e2ff',
+          200: '#e3b5ff',
+          300: '#d085ff',
+          400: '#b643ff',
+          500: '#9c00ff',
+          600: '#7d00cc',
+          700: '#5a0093',
+          800: '#3b0060',
+          900: '#180026',
+        }
+      }
+    },
   },
   plugins: [],
 }
